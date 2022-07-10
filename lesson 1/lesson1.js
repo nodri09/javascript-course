0824219b16fe03815847c8df8e86eb04bd1ee919
Nodar <nodar01@gmail.com>
2022-07-10 20:14:04 +0400

    finished lesson 1 tasks

--- lesson 1/lesson1.js
+++ lesson 1/lesson1.js
@@ -51,4 +51,45 @@ if (anotherUser.age > 18 && anotherUser.studentstatus === 'active' ){
 }
 
 // Task 5.
-// To Do
+// Define some array
+let someArray = ['watermelon', 'pear', 10, 34, 50, 'apple', 'pinepple'];
+
+// If the type of some array element is string than print it out
+console.log(`\nTask 5.\nIf the type of some array [${someArray}] element is string than print it out:`)
+
+for (i = 0; i < someArray.length; i++){
+    if (typeof(someArray[i]) === 'string'){
+        console.log(someArray[i]);
+    }
+}
+
+
+// Task 6.
+// Define number's multilevel array
+let numArray = [[2, -3, 5, 10], [25, -24, 11, 100], [-6, -7, 10]];
+
+// Iterate through number's array and print out positive numbers.
+console.log(`\nTask 6.\nIterate through number's [${numArray}] array and print out positive numbers:`) 
+for (i = 0; i < numArray.length; i++){
+    for (j = 0; j < numArray[i].length; j++){
+        if (numArray[i][j] > 0){
+            console.log(numArray[i][j]);
+        }
+    }
+}
+
+// Task 7.
+// Define array from which you will take out numbers which are module of 5
+let moduleArray = [32, 10, 'hello', null, 'hello2', 50];
+
+console.log(`\nTask 6. \nPrint out only numbers which are modules of 5 from module array [${moduleArray}]`);
+
+for (i = 0; i < moduleArray.length; i++){
+    if (moduleArray[i] === null) {
+        moduleArray[i] = undefined;
+    }
+
+    if ((moduleArray[i] % 5) === 0){
+        console.log(`${moduleArray[i]}`);
+    }
+}
